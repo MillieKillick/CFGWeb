ab7aad8cd3d7ec567a3cd3fc27c42f306db86596
MillieKillick <132579626+MillieKillick@users.noreply.github.com>
2023-06-22 14:04:39 +0100

    dog calculator

--- script.js
+++ script.js
@@ -12,8 +12,21 @@ menuClose.addEventListener("click", () => {
   overlay.classList.remove("overlay--active");
 });
 
+//Dog Calculator 2
+$('.num-age').keyup(function(e) {
+  var num, smallAge;
 
+  num = $(this).val();
 
+  if (!$.isNumeric(num)) {
+    return;
+  }
 
+  smallAge = (num * 7) + " years old in dog years!";
+
+  $('.total-small-dog').html(smallAge);
+  $('.total-medium-dog').html(mediumAge);
+  $('.total-big-dog').html(bigAge);
+});
 
 
